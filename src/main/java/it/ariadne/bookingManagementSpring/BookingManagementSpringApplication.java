2c83a8fe30a2256bd9d237e424acafcd3dcb44ed
Federica Amato <fede9423@live.it>
2018-11-30 17:53:35 +0100

    Added servletcomponentscan to recognise servlets

--- src/main/java/it/ariadne/bookingManagementSpring/BookingManagementSpringApplication.java
+++ src/main/java/it/ariadne/bookingManagementSpring/BookingManagementSpringApplication.java
@@ -2,7 +2,9 @@ package it.ariadne.bookingManagementSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages= {"it.ariadne.bookingManagementSpring.controller"})
 @SpringBootApplication
 public class BookingManagementSpringApplication {
 
