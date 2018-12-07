a05e0bee17edfef19f4fc8fe5c6c2222e4124d83
Federica Amato <fede9423@live.it>
2018-12-07 12:44:09 +0100

    Added findbyAppUser

--- src/main/java/it/ariadne/bookingManagementSpring/dao/BookingsDAO.java
+++ src/main/java/it/ariadne/bookingManagementSpring/dao/BookingsDAO.java
@@ -1,11 +1,14 @@
 package it.ariadne.bookingManagementSpring.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import it.ariadne.bookingManagementSpring.entity.AppUser;
 import it.ariadne.bookingManagementSpring.entity.Bookings;
 
 @Repository
 public interface BookingsDAO extends CrudRepository<Bookings, Long>{
-
+	 public List<Bookings> findByAppUser(AppUser app);
 }
