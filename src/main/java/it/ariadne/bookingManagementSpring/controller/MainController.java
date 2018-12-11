d064b0da4761ebefdb97e62ffe21e5e29067e22a
Federica Amato <fede9423@live.it>
2018-12-11 12:54:13 +0100

    Added security on accesses

--- src/main/java/it/ariadne/bookingManagementSpring/controller/MainController.java
+++ src/main/java/it/ariadne/bookingManagementSpring/controller/MainController.java
@@ -35,22 +35,22 @@ import it.ariadne.bookingManagementSpring.utils.WebUtils;
 
 @Controller
 public class MainController {
-	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/admin/home" }, method = RequestMethod.GET)
 	public String homePage(Model model) {
 		return "homePage";
 	}
 
-	@RequestMapping(value = { "/prenotazioni" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/admin/prenotazioni" }, method = RequestMethod.GET)
 	public String prenotazioni(Model model) {
 		return "prenotazioni";
 	}
 
-	@RequestMapping(value = { "/richieste" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/admin/richieste" }, method = RequestMethod.GET)
 	public String richieste(Model model) {
 		return "richieste";
 	}
 
-	@RequestMapping(value = { "/risorse" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/admin/risorse" }, method = RequestMethod.GET)
 	public String risorse(Model model) {
 		return "risorse";
 	}
